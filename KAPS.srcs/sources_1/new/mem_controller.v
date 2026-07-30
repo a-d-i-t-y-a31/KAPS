75e075558f8e8c0f8e9bcf811329680c99c0a3eb
aditya-89-e <adityasri@kgpian.iitkgp.ac.in>
2026-07-30 23:51:12 +0530

    Initial structure with ram.v and mem_controller.v

---
+++ KAPS.srcs/sources_1/new/mem_controller.v
@@ -0,0 +1,118 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/29/2026 10:01:58 PM
+// Design Name: 
+// Module Name: mem_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mem_controller(
+    input             clk,
+    input             clk_bar,
+    input             rst,
+    input             cs_bar,
+    input      [1:0]  r_w_bar,        
+    input      [23:0] address,        
+    input      [7:0]  data,           
+    inout      [7:0]  in_out_line,   
+    output reg        read_write_ram
+);
+
+    reg [2:0] state, next_state;
+    
+    
+    reg [7:0] in_out_driver;
+    reg       oe; 
+
+    
+    assign in_out_line = oe ? in_out_driver : 8'bZZZZZZZZ;
+
+    
+    always @(*) begin
+        case(state)
+            3'd0: next_state = 3'd1;
+            3'd1: next_state = 3'd2;
+            3'd2: next_state = 3'd3;
+            3'd3: next_state = 3'd4;
+            3'd4: next_state = 3'd5;
+            3'd5: next_state = 3'd0;
+            default: next_state = 3'd0;
+        endcase
+    end
+
+    
+
+    always @(posedge clk) begin
+        if (rst)
+            state <= 3'd0;
+        else if (!cs_bar)
+            state <= next_state;
+    end 
+
+    // 3. Output Logic (Combinational)
+    always @(*) begin
+        // Default values to prevent unwanted latches
+        read_write_ram = 1'b0;
+        in_out_driver  = 8'b0;
+        oe             = 1'b1; // Default driving bus
+
+        case(state)
+            3'd0: begin
+                in_out_driver  = address[23:16];
+                read_write_ram = 1'b1;
+            end
+            3'd1: begin
+                in_out_driver  = address[15:8];
+                read_write_ram = 1'b1;
+            end
+            3'd2: begin
+                in_out_driver  = address[7:0];
+                read_write_ram = 1'b1;
+            end
+            3'd3: begin
+                in_out_driver  = 8'b0;
+                read_write_ram = 1'b0;
+            end
+            3'd4: begin
+                if (r_w_bar[1]) begin
+                    read_write_ram = 1'b0;
+                    oe             = 1'b0; 
+                end else begin
+                    in_out_driver  = data;
+                    read_write_ram = 1'b1;
+                    oe             = 1'b1;
+                end
+            end
+            3'd5: begin
+                if (r_w_bar[0]) begin
+                    read_write_ram = 1'b0;
+                    oe             = 1'b0; 
+                end else begin
+                    in_out_driver  = data;
+                    read_write_ram = 1'b1;
+                    oe             = 1'b1;
+                end
+            end
+            default: begin
+                read_write_ram = 1'b0;
+                in_out_driver  = 8'b0;
+                oe             = 1'b0;
+            end
+        endcase
+    end
+
+endmodule
---
+++ KAPS.srcs/sources_1/new/ram.v
@@ -0,0 +1,75 @@
+module ram(
+    input clk,
+    input clk_bar,
+    input rset,
+    input cs_bar,
+    input rw_bar,
+    inout [7:0] data
+);
+    reg [7:0] ram_data [0:33554431];
+    reg [24:0] addr;
+    reg [2:0] state;
+
+
+    localparam ADDR_BYTE2 = 3'd0,
+               ADDR_BYTE1 = 3'd1,
+               ADDR_BYTE0 = 3'd2,
+               BLANK      = 3'd3,
+               DATA_1   = 3'd4,
+               DATA_2  = 3'd5;
+
+    assign data = (cs_bar == 0 && rw_bar == 1) ? (state == DATA_1 ? ram_data[addr] : (state == DATA_2 ? ram_data[addr+1] : 8'bz)) : 8'bz;
+
+    always @(posedge clk) begin
+        if (cs_bar == 0) begin
+            if (rset == 1) begin
+                integer i;
+                
+                for (i = 0; i < 33554432; i = i + 1) begin          // FIND A BETTER WAY TO RESET THE RAM
+                    ram_data[i] = 8'b0;
+                end
+
+            end 
+            else begin
+                case (state)
+
+                    ADDR_BYTE2: begin               //STATE FOR CLK CYCLE 1
+                        addr[24:17] <= data;
+                        state <= ADDR_BYTE1;
+                    end
+                    ADDR_BYTE1: begin               //STATE FOR CLK CYCLE 2
+                        addr[16:9] <= data;
+                        state <= ADDR_BYTE0;
+                    end
+                    ADDR_BYTE0: begin               //STATE FOR CLK CYCLE 3
+                        addr[8:1] <= data;
+                        addr[0] <= 0;
+                        state <= BLANK;
+                    end
+
+                    BLANK: begin                    //STATE FOR CLK CYCLE 4     
+                        state <= DATA_1;
+                    end
+
+                    DATA_1: begin                   //STATE FOR CLK CYCLE 5
+                        if (rw_bar == 0) begin
+                            ram_data[addr] <= data;
+                        end 
+                        state <= DATA_2;
+                    end
+
+                    DATA_2: begin                   //STATE FOR CLK CYCLE 6
+                        if (rw_bar == 0) begin
+                            ram_data[addr+1] <= data;
+                        end 
+                        state <= ADDR_BYTE2;
+                    end
+
+                    default: begin                  //DEFAULT STATE
+                        state <= ADDR_BYTE2;
+                    end
+                endcase
+            end
+        end
+    end
+endmodule
